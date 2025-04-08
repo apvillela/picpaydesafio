f76e84e1d537e376e303d762e69656f38cd52781
Alexandre Villela <ap.villela10@gmail.com>
2025-04-08 20:34:16 -0300

    feat: first commit

---
+++ src/main/java/com/picpayDesafio/PicpayDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.picpayDesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioApplication.class, args);
+	}
+
+}
